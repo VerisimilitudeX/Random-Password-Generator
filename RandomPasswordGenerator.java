473dccf639c4b1d89e1d6e98302623d5e5b0a9ee
Piyush Acharya <acharyapiyush1@outlook.com>
2022-08-17 16:58:56 -0700

    Added a typing effect to the output messages

--- RandomPasswordGenerator.java
+++ RandomPasswordGenerator.java
@@ -1,33 +1,68 @@
 import java.util.Scanner;
 
-// create a random password generator that takes input from the user on how long the password should be and how many passwords it should return and then generates a random password of that length and number. It should have letters, numbers, and symbols and must sounds like words in the english dictionary.
-    public class RandomPasswordGenerator {
-        // create a random password generator that takes input from the user on how long the password should be and how many passwords it should return and then generates a random password of that length and number. It should have letters, numbers, and symbols
-        public static String generateRandomCharacter() {
-            String randomCharacter = "";
-            int randomNumber = (int) (Math.random() * 94) + 33;
-            randomCharacter = Character.toString((char) randomNumber);
-            return randomCharacter;
-        }
-        public static String generatePassword(int length, int numberOfPasswords) {
-            String password = "";
-            for (int i = 0; i < numberOfPasswords; i++) {
-                for (int j = 0; j < length; j++) {
-                    password += generateRandomCharacter();
-                }
-                password += "\n";
+public class RandomPasswordGenerator {
+    // Generates a single random character (lowercase, uppercase, digit, or special
+    // character)
+    public static String generateRandomCharacter() {
+        String randomCharacter = "";
+        int randomNumber = (int) (Math.random() * 94) + 33;
+        randomCharacter = Character.toString((char) randomNumber);
+        return randomCharacter;
+    }
+
+    // Generates a random password of the length and number specified by the user
+    public static String generatePassword(int length, int numberOfPasswords) {
+        String password = "";
+        for (int i = 0; i < numberOfPasswords; i++) {
+            for (int j = 0; j < length; j++) {
+                password += generateRandomCharacter();
             }
-            return password;
+            password += "\n";
         }
-        public static void main(String[] args) {
-            try (Scanner sc = new Scanner(System.in)) {
-                System.out.println("How long do you want your password to be?");
-                int length = sc.nextInt();
-                System.out.println("How many passwords do you want?");
-                int number = sc.nextInt();
-                for (int i = 0; i < number; i++) {
-                    System.out.println(generatePassword(length, number));
+        return password;
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            // Ask the user for the length of the password
+            String message = "How long do you want your password to be?\n";
+            for (char c : message.toCharArray()) {
+                System.out.print(c);
+                try {
+                    Thread.sleep(35);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
+            int length = sc.nextInt();
+            System.out.println();
+
+            // Ask the user for the number of passwords to generate
+            message = "How many passwords do you want?\n";
+            for (char c : message.toCharArray()) {
+                System.out.print(c);
+                try {
+                    Thread.sleep(35);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            int number = sc.nextInt();
+            System.out.println();
+
+            // Outputs the generated passwords
+            for (int i = 0; i < 50; i++) {
+                System.out.println();
+                try {
+                    Thread.sleep(6);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("\nHere are your random passwords:");
+            System.out.println("--------------------------------");
+            System.out.println(generatePassword(length, number) + "\n");
         }
     }
+}
